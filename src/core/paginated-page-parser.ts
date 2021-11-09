bfcbf43f4a47513b54f9e84b75af03135bc311b5
Supamiu <contact@flavien-normand.fr>
2021-11-09 14:22:58 +0100

    fet(parser): new PaginatedPageParser

---
+++ src/core/paginated-page-parser.ts
@@ -0,0 +1,21 @@
+import {PageParser} from "./page-parser";
+import {Request} from "express";
+
+export abstract class PaginatedPageParser extends PageParser {
+
+    protected abstract getBaseURL(req: Request): string;
+
+    protected getURL(req: Request): string {
+        let query = '';
+        if (req.query && req.query.page) {
+            query = `?page=${req.query?.page}`;
+        }
+        return `${this.getBaseURL(req)}${query}`;
+    }
+
+    async parse(req: Request, columnsPrefix: string = ''): Promise<Object> {
+        const baseParse: any = await super.parse(req, columnsPrefix);
+        delete baseParse['ListNextButton'];
+        return baseParse;
+    }
+}
--- src/profile/achievements.ts
+++ src/profile/achievements.ts
@@ -1,14 +1,14 @@
 import {Request} from "express";
 import {CssSelectorRegistry} from "../core/css-selector-registry";
 import * as achievements from '../lib/lodestone-css-selectors/profile/achievements.json';
-import {PageParser} from "../core/page-parser";
+import {PaginatedPageParser} from "../core/paginated-page-parser";
 
-export class Achievements extends PageParser {
+export class Achievements extends PaginatedPageParser {
     protected getCSSSelectors(): CssSelectorRegistry {
         return achievements;
     }
 
-    protected getURL(req: Request): string {
+    protected getBaseURL(req: Request): string {
         return "https://na.finalfantasyxiv.com/lodestone/character/" + req.params.characterId + "/achievement";
     }
 
